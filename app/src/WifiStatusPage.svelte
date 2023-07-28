8be18711f70b6c689c10b2ae2513fdad8edf198e
Jacob Jordan <doublejinitials@gmail.com>
2023-07-28 03:04:34 -0500

    Prevent changing wifi network if connected through the active network

--- app/src/WifiStatusPage.svelte
+++ app/src/WifiStatusPage.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import { SvgIcon, getWifiRssiIcon, WizardPageLayout } from "./ui";
     import { loadWifiStatus, wifiStatus } from "./store";
-    import { onMount } from "svelte";
+    import { createEventDispatcher, onMount } from "svelte";
+    import Modal from "./ui/Modal.svelte";
+    import Button from "./ui/Button.svelte";
 
     let signalStrength: string;
     $: if ($wifiStatus.connected) {
@@ -19,7 +21,19 @@
         }
     }
 
+    let changeNetworkDisabledModalOpen = false;
+
     const statusLabelClass = "text-xs font-medium text-white px-1.5 py-0.5 rounded uppercase";
+
+    const dispatch = createEventDispatcher<{ back: void, next: void }>();
+
+    function onBack() {
+        if ($wifiStatus.connected && window.location.hostname === $wifiStatus.ip) {
+            changeNetworkDisabledModalOpen = true;
+        } else {
+            dispatch("back");
+        }
+    }
 
     onMount(() => {
         const t = setInterval(loadWifiStatus, 5000);
@@ -27,7 +41,7 @@
     });
 </script>
 
-<WizardPageLayout title="Wifi Network" backButtonLabel="Change" on:next on:back nextDisabled={!$wifiStatus.connected}>
+<WizardPageLayout title="Wifi Network" backButtonLabel="Change" on:next on:back={onBack} nextDisabled={!$wifiStatus.connected}>
     <div class="w-full flex flex-col items-center">
         <SvgIcon icon={getWifiRssiIcon($wifiStatus.connected && $wifiStatus.rssi)} size="xl" />
         <h1 class="mt-2 mb-1 text-2xl font-medium break-all">{$wifiStatus.ssid}</h1>
@@ -50,3 +64,19 @@
         </div>
     {/if}
 </WizardPageLayout>
+
+{#if $wifiStatus.connected && changeNetworkDisabledModalOpen}
+    <Modal title="Cannot Change Network" size="xs">
+        <p class="text-md">
+            You are currently accessing this page through the '{$wifiStatus.ssid}' network, so it cannot be changed.
+        </p>
+        <p class="text-md">
+            If you want to use a different WiFi network, connect to the calendar setup WiFi instead by following the instructions
+            on the calendar's screen.
+        </p>
+        <div class="flex flex-row">
+            <span class="grow-[2]" />
+            <Button class="grow" on:click={() => changeNetworkDisabledModalOpen = false}>Ok</Button>
+        </div>
+    </Modal>
+{/if}
