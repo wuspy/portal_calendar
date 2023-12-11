dde9dfaf60636c12c64161afeb42f9d4c5856814
Jacob Jordan <doublejinitials@gmail.com>
2023-12-11 00:31:22 -0600

    Update favicon

--- app/src/ui/Spinner.svelte
+++ app/src/ui/Spinner.svelte
@@ -1,14 +1,11 @@
 <script context="module" lang="ts">
     import classNames from "classnames";
-
-    const path = "M10 77 L126 55 L66 15 A126 126 0 0 0 10 77";
-    const rotations = [45, 90, 135, 180, 225, 270, 315];
 </script>
 
 <svg fill="currentColor" {...$$props} class={classNames("spinner", $$props.class)} viewBox="0 0 252 252">
-    <path d={path} />
-    {#each rotations as rotation (rotation)}
-        <path d={path} transform="rotate({rotation}, 126, 126)" />
+    <path id="slice" d="M10 77 L126 55 L66 15 A126 126 0 0 0 10 77" />
+    {#each [45, 90, 135, 180, 225, 270, 315] as rotation}
+        <use href="#slice" transform="rotate({rotation}, 126, 126)" />
     {/each}
 </svg>
 
